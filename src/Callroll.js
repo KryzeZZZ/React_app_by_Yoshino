60d6bd155f176b454d7cf21e09ec2ef3bcce542a
Yoshino~ <mc200710@outlook.com>
2024-04-23 13:12:07 +0800

    first

--- src/Callroll.js
+++ src/Callroll.js
@@ -1,11 +1,27 @@
 import React, {useEffect, useState} from 'react';
 import {Button, SplitButtonGroup, Typography} from '@douyinfe/semi-ui';
 import { Layout } from '@douyinfe/semi-ui';
-import { List, Descriptions, ButtonGroup, Rating } from '@douyinfe/semi-ui';
+import { List, Descriptions, ButtonGroup, Rating, Modal } from '@douyinfe/semi-ui';
 import axios from 'axios';
 
 function CallRoll() {
+    const [visible, setVisible] = useState(false);
+    const showDialog = () => {
+        setVisible(true);
+    };
+    const handleOk = () => {
+        setVisible(false);
+        console.log('Ok button clicked');
+    };
+    const handleCancel = () => {
+        setVisible(false);
+        console.log('Cancel button clicked');
+    };
+    const handleAfterClose = () => {
+        console.log('After Close callback executed');
+    };
     const [data, setData] = useState([]);
+    const {Text, Title} = Typography;
     const [chooseList, setChooseList] = useState([]);
     useEffect(() => {
         axios.get('http://localhost:4000/Students')
@@ -21,7 +37,14 @@ function CallRoll() {
     function CallrollAction(StuList) {
         let chooseState = Math.random();
         setChooseList(StuList.filter((content, index) => index != parseInt(chooseState / (1 / StuList.length))))
-        return StuList[parseInt(chooseState / (1 / StuList.length))].name;
+        const outputName = new Promise((resolve, reject) => {
+            if(!StuList || StuList.length === 0) reject("None")
+            else {
+                showDialog()
+                resolve(StuList[parseInt(chooseState / (1 / StuList.length))].name)
+            }
+        })
+            .then(name => setChosenOne(name)).catch(name => setChosenOne(name))
     }
     const style = {
         border: '1px solid var(--semi-color-border)',
@@ -32,49 +55,44 @@ function CallRoll() {
     let [chosenOne, setChosenOne] = useState("None");
     const { Header, Footer, Sider, Content } = Layout;
     return (
-        <Layout>
-            <Header>Callroll System</Header>
-            <Layout>
-                <Sider>
-                    <div>
-                        <List
-                            grid={{
-                                gutter: 12,
-                                span: 6,
-                            }}
-                            dataSource={data}
-                            renderItem={item => (
-                                <List.Item style={style}>
-                                    <div>
-                                        <h3 style={{
-                                            color: 'var(--semi-color-text-0)',
-                                            fontWeight: 500
-                                        }}>{item.name}</h3>
-                                        <Descriptions
-                                            align="center"
-                                            size="small"
-                                            row
-                                            data={[
-                                                {key: '分数', value: item.points},
-                                            ]}
-                                        />
-                                    </div>
-                                </List.Item>
-                            )}
-                        />
-                    </div>
-                </Sider>
-                <Content>
-                    <div>
-                        <Button onClick={() => {
-                            setChosenOne(CallrollAction(chooseList))
-                        }}>点名</Button>
-                    </div>
-                    <Typography.Text>{chosenOne}</Typography.Text>
-                </Content>
-            </Layout>
+        <Layout className="components-layout-demo">
+            <Header>CallRoll System</Header>
+            <Content style={{height: 300, lineHeight: '300px'}}>
+                <List
+                    grid={{
+                        gutter: 12,
+                        span: 6,
+                    }}
+                    dataSource={data}
+                    renderItem={item => (
+                        <List.Item style={style}>
+                            <div>
+                                <h3 style={{ color: 'var(--semi-color-text-0)', fontWeight: 500 }}>{item.name}</h3>
+                                <Descriptions
+                                    align="center"
+                                    size="small"
+                                    row
+                                    data={[
+                                        { key: '分数', value: item.points },
+                                    ]}
+                                />
+                            </div>
+                        </List.Item>
+                    )}
+                />
+                <Button onClick={() => {setChosenOne(CallrollAction(chooseList))}}>点名</Button>
+                <Modal
+                    title="抽取成功"
+                    visible={visible}
+                    onOk={handleOk}
+                    afterClose={handleAfterClose} //>=1.16.0
+                    onCancel={handleCancel}
+                    closeOnEsc={true}
+                >
+                    {chosenOne}
+                </Modal>
+            </Content>
         </Layout>
-
     )
 
 }
